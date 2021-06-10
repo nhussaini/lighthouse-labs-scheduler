3f2d8dc8e2b8348d9710e37b8578553ab68ee6d3
unknown <nasrullah_hussaini@yahoo.com>
2021-06-09 17:00:15 -0700

    transform data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,32 @@
+export function getAppointmentsForDay(state, name) {
+  //I can also use find instead of filter
+  const filteredDays = state.days.filter(day => day.name === name);
+  // console.log("filteredDays: ",filteredDays);
+  if(state.days.length===0 || filteredDays.length===0){
+    return [];
+  }
+
+  //get the appointments
+  const appointmentsFromDays = filteredDays[0].appointments;
+  // const filtredAppointments = appointments.filter(appointment=>{
+  //   return 
+  // })
+  let filteredAppointments = [];
+  // console.log("appointments array: ",appointments);
+  // for( let key in state.appointments){
+  //   console.log("key inside appointments",key);
+  // }
+  for(let appointment of appointmentsFromDays) {
+    // console.log("appointment",appointment);
+    // filteredAppointments.push(appointment);
+    filteredAppointments.push(state.appointments[appointment]);
+    // for(let key in state.appointments) {
+    //   if(appointment == key) {
+    //     filteredAppointments.push(state.appointments[key]);
+    //   }
+    // }
+
+
+  }
+  return filteredAppointments;
+}
