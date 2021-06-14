b80c749f45de7ba8358babcc4448229acaeb0a15
unknown <nasrullah_hussaini@yahoo.com>
2021-06-14 15:38:03 -0700

    implement the test for the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import InterviewerList from "components/InterviewerList"
 export default function Form(props) {
   const [interviewer, setInterviewer]= useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
+  const [error, setError] = useState("");
   
   const reset = function(){
     setName("");
@@ -13,6 +14,16 @@ export default function Form(props) {
   const cancel = function(){
     reset();
     props.onCancel();
+  }
+
+  //to validate that the name input cannot be empty
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
   }
   
   return(
@@ -24,20 +35,21 @@ export default function Form(props) {
           name="name"
           type="text"
           placeholder="Enter Student Name"
+          // placeholder = {name ? name : "Enter student Name"}
           value={name}
           onChange={(event) => setName(event.target.value)}
-          
-          /*
-            This must be a controlled component
-          */
+          data-testid="student-name-input"
          />
+        <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList interviewers={props.interviewers} interviewer={interviewer} onChange={setInterviewer} />
         </section>
         <section className="appointment__card-right">
           <section className="appointment__actions">
             <Button danger onClick={cancel}>Cancel</Button>
-            <Button confirm onClick={()=>props.onSave(name, interviewer)} >Save</Button>
+            {/* <Button confirm onClick={()=>props.onSave(name, interviewer)} >Save</Button> */}
+            <Button confirm onClick={event => validate()} >Save</Button>
+
            {/* <Button confirm onClick={validate}>
             Save
            </Button> */}
