fd4ba3fb0c3dee2fd4c9eec0e8b68804f620934d
unknown <nasrullah_hussaini@yahoo.com>
2021-06-15 17:15:41 -0700

    implement debugging tests

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -84,7 +84,7 @@ export default function Appointment(props) {
          
       )}
       {mode === SAVING && (
-        <Status />
+        <Status message="Saving"/>
       )}
        {mode === CONFIRM && (
         <Confirm
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement,fireEvent,getByText, prettyDOM,getAllByTestId,getByAltText,getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement,fireEvent,getByText, prettyDOM,getAllByTestId,getByAltText,getByPlaceholderText,queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -42,7 +42,7 @@ describe("Application", () => {
 //   })
 
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-  const { container } = render(<Application />);
+  const { container,debug } = render(<Application />);
 
   await waitForElement(() => getByText(container, "Archie Cohen"));
 
@@ -58,6 +58,20 @@ it("loads data, books an interview and reduces the spots remaining for Monday by
   fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
   fireEvent.click(getByText(appointment, "Save"));
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();//not working
+
+  await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
+  expect(getByText(appointment, "Lydia Miller-Jones")).toBeInTheDocument();
+
+  //Find the specific day node that contains the text "Monday"
+  const day = getAllByTestId(container, "day").find(day => {
+    return queryByText(day, "Monday")
+  })
+  console.log("day====>",day);
+
+
+  // expect(getByText(day, "Monday")).toBeInTheDocument();
+  expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 
   console.log(prettyDOM(appointment));
 });
